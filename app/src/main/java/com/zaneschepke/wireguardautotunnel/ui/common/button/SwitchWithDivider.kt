34edc47afe9e1bc8add0c20376d39b7aa0344a02
zaneschepke <dev@zaneschepke.com>
2026-07-23 03:03:03 -0400

    refactor: improve UI divider with a chevron
    Match Android to denote another deeper screen

--- app/src/main/java/com/zaneschepke/wireguardautotunnel/ui/common/button/SwitchWithDivider.kt
+++ app/src/main/java/com/zaneschepke/wireguardautotunnel/ui/common/button/SwitchWithDivider.kt
@@ -8,6 +8,10 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.outlined.ChevronRight
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.VerticalDivider
 import androidx.compose.runtime.Composable
@@ -19,19 +23,31 @@ import androidx.compose.ui.unit.dp
 @Composable
 fun SwitchWithDivider(
     checked: Boolean,
+    modifier: Modifier = Modifier,
     onClick: (Boolean) -> Unit,
     enabled: Boolean = true,
-    modifier: Modifier = Modifier,
 ) {
     Row(
         modifier = Modifier.height(IntrinsicSize.Min),
         verticalAlignment = Alignment.CenterVertically,
         horizontalArrangement = Arrangement.spacedBy(12.dp),
     ) {
-        VerticalDivider(
+        Row(
             modifier = Modifier.fillMaxHeight().padding(horizontal = 8.dp, vertical = 8.dp),
-            color = MaterialTheme.colorScheme.outline,
-        )
+            verticalAlignment = Alignment.CenterVertically,
+            horizontalArrangement = Arrangement.spacedBy(6.dp),
+        ) {
+            Icon(
+                Icons.Outlined.ChevronRight,
+                null,
+                tint = MaterialTheme.colorScheme.outline,
+                modifier = Modifier.size(24.dp),
+            )
+            VerticalDivider(
+                modifier = Modifier.fillMaxHeight(),
+                color = MaterialTheme.colorScheme.outline,
+            )
+        }
         Box(modifier = Modifier.pointerInput(Unit) { detectTapGestures {} }) {
             ThemedSwitch(
                 checked = checked,
